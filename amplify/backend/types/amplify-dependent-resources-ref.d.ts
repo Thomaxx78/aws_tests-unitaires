9c23d90c5b8dd1dc1be62f243233518d37ec4bca
Thomas Filhol <thomasfilhol@MacBook-Air-de-Thomas-3.local>
2025-07-03 11:33:49 +0200

    Setup project

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,21 @@
+export type AmplifyDependentResourcesAttributes = {
+  "function": {
+    "awstestsunid03afd6b": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "dynamo3a4ecaa7": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "StreamArn": "string"
+    }
+  }
+}
