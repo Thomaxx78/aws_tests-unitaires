fc0eb8b3b4186caf011150931e81bf7da86ecbbb
Thomas Filhol <thomasfilhol@MacBook-Air-de-Thomas-3.local>
2025-07-03 12:01:09 +0200

    Eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'eslint/config'
 import globals from 'globals'
 import js from '@eslint/js'
-import pluginVue from 'eslint-plugin-vue'
-import pluginVitest from '@vitest/eslint-plugin'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 export default defineConfig([
   {
@@ -27,14 +24,6 @@ export default defineConfig([
         ...globals.browser,
       },
     },
-  },
-
-  js.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
-  
-  {
-    ...pluginVitest.configs.recommended,
-    files: ['src/**/__tests__/*'],
   },
 
   {
